8b36bf3449ba5f0ca1577195bfee6e5b2d11d3ca
Igor Shevchuk <igorshevchuk111@gmail.com>
2024-10-02 16:12:59 +0100

    Toasts Notifications

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import PageNotFound from './pages/PageNotFound';
 import GlobalStyles from './styles/GlobalStyles';
 import AppLayout from './ui/AppLayout';
 import Cabins from './pages/Cabins';
+import { Toaster } from 'react-hot-toast';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -42,6 +43,22 @@ function App() {
             <Route path="*" element={<PageNotFound />} />
           </Routes>
         </BrowserRouter>
+        <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: '8px' }}
+          toastOptions={{
+            success: { duration: 3000 },
+            error: { duration: 5000 },
+            style: {
+              fontSize: '16px',
+              maxWidth: '500px',
+              padding: '16px  24px',
+              backgroundColor: 'var(--color-gray-0)',
+              color: 'var(--color-gray-700)',
+            },
+          }}
+        />
       </QueryClientProvider>
     </>
   );
