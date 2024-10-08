c3740a0372354af57851cd44430b5f7753b9ab45
Igor Shevchuk <igorshevchuk111@gmail.com>
2024-10-08 12:08:05 +0100

    Create new Cabin and upload image on Supabase

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,5 +1,7 @@
 import supabase from './supabase';
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+
 export async function getCabins() {
   const { data, error } = await supabase.from('cabins').select('*');
 
@@ -11,14 +13,30 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`;
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
   const { data, error } = await supabase
     .from('cabins')
-    .insert([newCabin])
+    .insert([{ ...newCabin, image: imagePath }])
     .select();
 
   if (error) {
     console.error(error);
     throw new Error('Cabins could not be created');
+  }
+
+  const { error: storageError } = await supabase.storage
+    .from('cabin-images')
+    .upload(imageName, newCabin.image);
+  if (storageError) {
+    await supabase.from('cabins').delete().eq('id', data.id);
+    console.error(storageError);
+    throw new Error(
+      'Cabins image could not be uploaded and the cabin was not created'
+    );
+  } else {
+    // Handle success
   }
 
   return data;
