6f29a5eace74d428537ef6ed3a16d9ddb8853262
Igor Shevchuk <igorshevchuk111@gmail.com>
2024-11-05 22:35:29 +0000

    Compound Table Component

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -5,18 +5,7 @@ import EditCabin from './EditCabin';
 import DeleteCabin from './DeleteCabin';
 import DuplicateCabin from './DuplicateCabin';
 import Row from '../../ui/Row';
-
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
+import Table from '../../ui/Table';
 
 const Img = styled.img`
   display: block;
@@ -56,7 +45,7 @@ function CabinRow({ cabin }) {
   } = cabin;
 
   return (
-    <TableRow role="row">
+    <Table.Row>
       <Img src={image} />
       <Cabin>{name}</Cabin>
       <div> Fits up to {maxCapacity} guests</div>
@@ -71,7 +60,7 @@ function CabinRow({ cabin }) {
         <EditCabin cabin={cabin} />
         <DeleteCabin imagePath={image} cabinId={cabinId} />
       </Row>
-    </TableRow>
+    </Table.Row>
   );
 }
 
