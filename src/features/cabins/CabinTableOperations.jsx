f7facdd2f329f033f52a2b971f7dad47c7681799
Igor Shevchuk <igorshevchuk111@gmail.com>
2024-11-07 13:55:18 +0000

    Filter Cabin Table

---
+++ src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,18 @@
+import TableOperations from '../../ui/TableOperations';
+import Filter from '../../ui/Filter';
+
+function CabinTableOperations() {
+  const optionsData = [
+    { value: 'all', label: 'All' },
+    { value: 'no-discount', label: 'No discount' },
+    { value: 'with-discount', label: 'With discount' },
+  ];
+
+  return (
+    <TableOperations>
+      <Filter filterField="discount" options={optionsData} />
+    </TableOperations>
+  );
+}
+
+export default CabinTableOperations;
