a518e822fe88b95bcba6172c47e8b96d1b571f51
Igor Shevchuk <igorshevchuk111@gmail.com>
2024-12-04 10:34:22 +0000

    Displaying Line Chart With the Recharts Library

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,12 +4,18 @@ import { useRecentStays } from './useRecentStays';
 import { useRecentBookings } from './useRecentBookings';
 import Spinner from '../../ui/Spinner';
 import { useCabins } from '../cabins/useCabins';
+import SalesChart from './SalesChart';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
+
+  @media (max-width: 1200px) {
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto auto 34rem auto;
+  }
 `;
 
 function DashboardLayout() {
@@ -27,6 +33,9 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
+      <div>d</div>
+      <div>d</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
