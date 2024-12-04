b203c16bec6126c823a492251101c083fee67a07
Igor Shevchuk <igorshevchuk111@gmail.com>
2024-12-04 18:02:38 +0000

    Displaying Stays for current day

--- src/features/dashboard/DurationChart.jsx
+++ src/features/dashboard/DurationChart.jsx
@@ -142,7 +142,6 @@ function DurationChart({ confirmedStays }) {
   const { isDarkMode } = useDarkMode();
   const startData = isDarkMode ? startDataDark : startDataLight;
   const data = prepareData(startData, confirmedStays);
-  console.log(data, 'data');
 
   return (
     <ChartBox>
